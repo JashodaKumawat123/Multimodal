b02489c20e4e19ef03476356834c0f5f1e512ec9
JashodaKumawat123 <119126891+JashodaKumawat123@users.noreply.github.com>
2025-05-13 15:15:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from router import route_query
 
-print("🧠 Welcome to the Multimodal SMoL-NLP Assistant! Type 'exit' to quit.")
+print("Welcome to the Multimodal SMoL-NLP Assistant! Type 'exit' to quit.")
 while True:
     user_input = input("You: ")
     if user_input.lower() == "exit":
